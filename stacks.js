062887c81c9a452ae3f485313637a110ced7de51
BenjaminSwan <61973811+BenjaminSwan@users.noreply.github.com>
2020-03-09 12:27:09 +0000

    init

---
+++ stacks.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+    function changeBanner(){
+        var img = new array
+
+        img[0] = document.images[0].src
+        img[1] = document.images[1].src
+        img[2] = document.images[2].src
+
+        var currentimg = img[0]
+
+        if(currentimg == img[0]) {
+            currentimg = img[1]
+        }
+
+        if(currentimg == img[1]) {
+            currentimg = img[2]
+        }
+    }
+</script>
